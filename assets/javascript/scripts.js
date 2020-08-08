0b427500e3e2bcecb8452da2cc02d79c3ea0fcd7
jarnalyrkar <joachimsolstrom@gmail.com>
2020-08-08 19:58:28 +0200

    Add new webpack, delete old stuff, update some templates

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,6 @@
+import MobileMenu from './modules/MobileMenu';
+// import bootstrap from '~/bootstrap/dist/js/bootstrap.bundle.min';
+// import bootstrapSlim from '~/jquery/dist/jquery.slim.min';
+
+/* Instansier objekter */
+const mobileMenu = new MobileMenu();
--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+// Only used by WooCommerce, the rest of the site uses base.twig.
 $context = Timber::context();
 $context['post'] = new Timber\Post();
 
