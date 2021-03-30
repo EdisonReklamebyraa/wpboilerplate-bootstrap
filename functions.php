c0ee71ea6d5fff88a51a4ca5e8039205220d866d
jarnalyrkar <joachimsolstrom@gmail.com>
2021-03-30 13:19:29 +0200

    Fork away woo-support

--- functions.php
+++ functions.php
@@ -2,7 +2,6 @@
 
 $base_dir = get_stylesheet_directory();
 // Enqueue dist/css and dist/js
-//   do not change this without also changing gulpfile.js and webpack.config.js to match
 require_once($base_dir . '/inc/enqueue_scripts.php');
 // Remove emojis
 require_once($base_dir . '/inc/disable_emojis.php');
@@ -18,5 +17,3 @@ require_once($base_dir . '/inc/add_theme_assets.php');
 require_once($base_dir . '/inc/add_twig_nav.php');
 // Add breadcrumb
 require_once($base_dir . '/inc/add_breadcrumb.php');
-// Add woocommerce-support
-// require_once($base_dir . '/inc/add_woocommerce_support.php');
