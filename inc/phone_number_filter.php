e46fc01b74227269b413836bc1582fe47f295dde
Joachim Strøm <joachimsolstrom@gmail.com>
2021-04-20 21:45:53 +0200

    Fix phone filter

--- inc/phone_number_filter.php
+++ inc/phone_number_filter.php
@@ -4,7 +4,7 @@
 function phone( $number ) {
   $number = str_replace(' ', '', $number);
   if (substr($number, 0, 1) == 4 || (substr($number, 0 , 1) == 9)) {
-    return substr($number, 0, 3) . " " . substr($number, 2, 2) . " " . substr($number, 4, 3);
+    return substr($number, 0, 3) . " " . substr($number, 3, 2) . " " . substr($number, 5, 3);
   }
   return substr($number, 0, 2) . " " . substr($number, 2, 2) . " " . substr($number, 4, 2) . " " . substr($number, 6, 2);
 }
