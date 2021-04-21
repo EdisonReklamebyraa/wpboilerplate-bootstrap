4ee2a29c381fc906b2fdba0c6a21fa710dc49029
Joachim Strøm <joachimsolstrom@gmail.com>
2021-04-21 10:06:56 +0200

    Cleanup require-statements

--- functions.php
+++ functions.php
@@ -1,19 +1,19 @@
 <?php
 
-$base_dir = get_stylesheet_directory();
+$base_dir = get_stylesheet_directory() . '/inc/';
 // Enqueue dist/css and dist/js
-require_once($base_dir . '/inc/enqueue_scripts.php');
+require_once $base_dir . 'enqueue_scripts.php';
 // Remove emojis
-require_once($base_dir . '/inc/disable_emojis.php');
+require_once $base_dir . 'disable_emojis.php';
 // Remove jquery by default
-require_once($base_dir . '/inc/deregister_jquery.php');
+require_once $base_dir . 'deregister_jquery.php';
 // Add markup to allow for responsive oembeds
-require_once($base_dir . '/inc/add_responsive_embed.php');
+require_once $base_dir . 'add_responsive_embed.php';
 // Extend Twig-functionality
-require_once($base_dir . '/inc/extend_twig.php');
+require_once $base_dir . 'extend_twig.php';
 // Add theme assets
-require_once($base_dir . '/inc/add_theme_assets.php');
+require_once $base_dir . 'add_theme_assets.php';
 // Add Twig-navigation
-require_once($base_dir . '/inc/add_twig_nav.php');
+require_once $base_dir . 'add_twig_nav.php';
 // Add breadcrumb
-require_once($base_dir . '/inc/add_breadcrumb.php');
+require_once $base_dir . 'add_breadcrumb.php';
