c2a9b16ca7104945b106e300993f6b28a89b8071
Joachim Strøm <joachimsolstrom@gmail.com>
2021-04-21 06:56:58 +0200

    Only needed for WooCommerce

--- header.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* Only used by WooCommerce, the rest of the site uses base.twig */
-$context = Timber::context();
-$context['post'] = new Timber\Post();
-
-Timber::render('partials/header.twig', $context);
